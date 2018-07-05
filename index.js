2a47a75fe4af61ec6f76535ff8fa2a27fb1fca98
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-05 12:26:15 +0100

    Add google Geocharts

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// alert('Boooooooommmmmm')
-//
-
 $.ajaxSetup({beforeSend: function(xhr){
   if (xhr.overrideMimeType)
   {
@@ -11,4 +8,29 @@ $.ajaxSetup({beforeSend: function(xhr){
 
 $.getJSON("./Country-Code-Mapping2.json", function(json) {
     console.log(json); // this will show the info it in firebug console
-}, "json");
+    google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+    google.charts.setOnLoadCallback(drawRegionsMap);
+
+    function drawRegionsMap() {
+      var data = google.visualization.arrayToDataTable([
+        ['Country', 'Popularity'],
+        ['Germany', 200],
+        ['United States', 300],
+        ['Brazil', 400],
+        ['Canada', 500],
+        ['France', 600],
+        ['RU', 700]
+      ]);
+
+      var options = {};
+
+      var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+      chart.draw(data, options);
+  }
+});
